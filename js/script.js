3f572761c214ed711dabda58731ff670206e4387
defaziofederico <defaz86@gmail.com>
2019-10-19 14:24:06 +0200

    b

--- js/script.js
+++ js/script.js
@@ -26,19 +26,12 @@ var Loc = /** @class */ (function () {
         this.city = city;
     }
     Loc.prototype.display = function () {
-        return this.location + "," + this.description + "," + this.adress + "," + this.zipcode + "," + this.img + "," + this.city;
-        display();
-        {
-            return $;
-            {
-                " <div class=\"media  p-3\">\n        <img src=" + this.img + " alt=\"\" class=\"mr-3 mt-3 rounded-circle\" style=\"width:150px;height:150px;\">\n        <div class=\"media-body\">\n            <div class=\"text-container pl-5 pt-10\">\n                <h4>" + this.location + "</h4>\n                // <p>" + this.description + "</p>\n                 <div id=\"place-type\">\n                 " + this.adress + this.zipcode + this.city + "\n                 </div>\n            </div>\n        </div>";
-            }
-        }
+        return "<div class=\"media  p-3\">\n        <img src=" + this.img + " alt=\"\" class=\"mr-3 mt-3 rounded-circle\" style=\"width:150px;height:150px;\">\n        <div class=\"media-body\">\n            <div class=\"text-container pl-5 pt-10\">\n                <h4>" + this.location + "</h4><br>\n                 <p>" + this.description + "</p>\n                 <div id=\"place-type\">\n                 " + this.adress + "<br>" + this.zipcode + "<br>" + this.city + "\n                 </div>\n            </div>\n        </div>";
     };
     return Loc;
 }());
-var carl = new Loc("Karlsplatz", "T", "Kreuzherrengasse 1", 1040, "../img/karl.jpg,", "Vienna");
-var zoo = new Loc("Schoenbrunn Zoo", "A", "Maxingstrasse 13b", 1130, "../img/zoo.jpg", "Vienna");
+var carl = new Loc("Karlsplatz", "This square, in English St.Charles Square, is a town square on the border of the first and fourth districts of Vienna. It is one of the most frequented and best connected transportation hubs in Vienna.", "Kreuzherrengasse 1", 1040, "../img/karl.jpg", "Vienna");
+var zoo = new Loc("Schoenbrunn Zoo", "A trip to Sch\u00F6nbrunn Zoo. From penguins and orangutans to giant pandas: discover more than 700 species of animals in the unique setting of this UNESCO wonder. ", "Maxingstrasse 13b", 1130, "../img/zoo.jpg", "Vienna");
 var arrLoc = [carl, zoo];
 for (var i = 0; i < arrLoc.length; i++) {
     document.getElementsByClassName("loc-container")[i].innerHTML += arrLoc[i].display();
@@ -52,12 +45,12 @@ var Restaurants = /** @class */ (function (_super) {
         return _this;
     }
     Restaurants.prototype.display = function () {
-        return this.location + "," + this.description + "," + this.adress + "," + this.zipcode + "," + this.img + "," + this.city + "," + this.tel + "," + this.website;
+        return "<div class=\"media  p-3\">\n        <img src=" + this.img + " alt=\"\" class=\"mr-3 mt-3 rounded-circle\" style=\"width:150px;height:150px;\">\n        <div class=\"media-body\">\n            <div class=\"text-container pl-5 pt-10\">\n                <h4>" + this.location + "</h4><br>\n                 <p>" + this.description + "</p>\n                 <div id=\"place-type\">\n                 " + this.adress + "<br>" + this.zipcode + "<br>" + this.city + "<br>" + this.tel + "<br>" + this.website + "\n                 </div>\n            </div>\n        </div>";
     };
     return Restaurants;
 }(Loc));
-var lemon = new Restaurants("Lemon", "A", "Kettenbr\u00FCckengasse 19", 1050, "../img/zoo.jpg", "Vienna", 0660443322, "www.lemon.at");
-var sixta = new Restaurants("Sixta", "T", "Sch\u00F6nbrunner Strasse 21", 1050, "../img/sixta.png", "Vienna", "0134523", "http://www.sixta.at");
+var lemon = new Restaurants("Lemon", "At Lemon we are constantly searching for the aromas and flavors of Austria. Join us in the world of Lemon, share our passion and let us present our country\u2019s harvest in the most delicious light.", "Kettenbr\u00FCckengasse 19", 1050, "../img/lemon.png", "Vienna", "0660443322", "http://www.lemonleaf.at");
+var sixta = new Restaurants("Sixta", "The sensitivity between creative cuisine and a traditional, Viennese offer determines our cuisine. We attach great importance to product quality, production quality and the best possible taste. The changing menu takes seasonal specialities and delicacies into account.", "Sch\u00F6nbrunner Strasse 21", 1050, "../img/sixta.png", "Vienna", "0660443323", "http://www.sixta-restaurant.at/");
 var arrRest = [lemon, sixta];
 for (var i = 0; i < arrRest.length; i++) {
     document.getElementsByClassName("rest-container")[i].innerHTML += arrRest[i].display();
@@ -66,17 +59,18 @@ var Events = /** @class */ (function (_super) {
     __extends(Events, _super);
     function Events(location, description, adress, zipcode, img, city, tel, website, date, price) {
         var _this = _super.call(this, location, description, adress, zipcode, img, city, tel, website) || this;
+        _this.website = website;
         _this.date = date;
         _this.price = price;
         return _this;
     }
     Events.prototype.display = function () {
-        return this.location + "," + this.description + "," + this.adress + "," + this.zipcode + "," + this.img + "," + this.city + "," + this.website + "," + this.date + "," + this.price;
+        return "<div class=\"media  p-3\">\n        <img src=" + this.img + " alt=\"\" class=\"mr-3 mt-3 rounded-circle\" style=\"width:150px;height:150px;\">\n        <div class=\"media-body\">\n            <div class=\"text-container pl-5 pt-10\">\n                <h4>" + this.location + "</h4><br>\n                 <p>" + this.description + "</p>\n                 <div id=\"place-type\">\n                 " + this.adress + "<br>" + this.zipcode + "<br>" + this.city + "<br>" + this.website + "<br>" + this.date + "\n                 </div>\n            </div>\n        </div>";
     };
     return Events;
 }(Loc));
-var kris = new Events("Kris Kristofferson", "K", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", 1150, "./img/kris.jpg", "Vienna", "kriskristofferson.com/", "Fr., 15.11.2019. 20:00", "58,50 EUR");
-var lenny = new Events("Lenny Kravitz", "L", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", 1150, "./img/lenny.jpg", "Vienna", "lennykravitz.com", "Sat,09.12.2019,19:30", "47,80 EUR");
+var kris = new Events("Kris Kristofferson", "Kristoffer Kristofferson (born June 22, 1936) is an American singer-songwriter and actor. Among his songwriting credits are the songs \"Me and Bobby McGee\", \"For the Good Times\", \"Sunday Mornin' Comin' Down\", and \"Help Me Make It Through the Night\", all of which were hits for other artists.", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", 1150, "../img/kris.jpg", "Vienna", "", "http://kriskristofferson.com/", "Fr.,15.11.2019. 20:00", "58,50 EUR");
+var lenny = new Events("Lenny Kravitz", "Leonard Albert Kravitz (born May 26, 1964) is an American singer, songwriter, actor, record producer, and multi-instrumentalist. His \"retro\" style incorporates elements of rock, blues, soul, R&B, funk, jazz, reggae, hard rock, psychedelic, pop, folk, and ballads.", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", 1150, "../img/lenny.jpg", "Vienna", "", "http://www.lennykravitz.com/", "Sat,09.12.2019,19:30", "47,80 EUR");
 var arrEve = [lenny, kris];
 for (var i = 0; i < arrEve.length; i++) {
     document.getElementsByClassName("eve-container")[i].innerHTML += arrEve[i].display();
--- js/script.ts
+++ js/script.ts
@@ -16,26 +16,25 @@ constructor(location,description,adress,zipcode,img,city) {
        this.city=city;
    }    
    display() {
-       return`${this.location},${this.description},${this.adress},${this.zipcode},${this.img},${this.city}`;
-       display() {return ${` <div class="media  p-3">
+     return `<div class="media  p-3">
         <img src=${this.img} alt="" class="mr-3 mt-3 rounded-circle" style="width:150px;height:150px;">
         <div class="media-body">
             <div class="text-container pl-5 pt-10">
-                <h4>${this.location}</h4>
-                // <p>${this.description}</p>
+                <h4>${this.location}</h4><br>
+                 <p>${this.description}</p>
                  <div id="place-type">
-                 ${this.adress}${this.zipcode}${this.city}
+                 ${this.adress}<br>${this.zipcode}<br>${this.city}
                  </div>
             </div>
         </div>`
-   }}
+  }
   }
 
    
-var carl = new Loc (`Karlsplatz`,`T`, `Kreuzherrengasse 1`,1040,`../img/karl.jpg,`,`Vienna`);
+var carl = new Loc (`Karlsplatz`,`This square, in English St.Charles Square, is a town square on the border of the first and fourth districts of Vienna. It is one of the most frequented and best connected transportation hubs in Vienna.`,`Kreuzherrengasse 1`,1040,`../img/karl.jpg`,`Vienna`);
 
 var zoo = new Loc (`Schoenbrunn Zoo`,
-  `A`,`Maxingstrasse 13b`,1130,`../img/zoo.jpg`,`Vienna`);
+  `A trip to Schönbrunn Zoo. From penguins and orangutans to giant pandas: discover more than 700 species of animals in the unique setting of this UNESCO wonder. `,`Maxingstrasse 13b`,1130,`../img/zoo.jpg`,`Vienna`);
 
 var arrLoc= [carl,zoo]; 
 for (let i = 0; i < arrLoc.length; i++) {
@@ -44,47 +43,70 @@ for (let i = 0; i < arrLoc.length; i++) {
 }  
 
 class Restaurants extends Loc  {
-   tel:"";
-   website:"";
+tel:"";
+website:"";
 
   
-constructor(location, description,adress,zipcode,img,city,tel,website) {
-    super(location,description,adress,zipcode,img, city) 
-       this.tel=tel;
-       this.website=website;
-   }
-   display() { 
-       return`${this.location},${this.description},${this.adress},${this.zipcode},${this.img},${this.city},${this.tel},${this.website}`;
+constructor(location,description,adress,zipcode,img,city,tel,website) {
+super(location,description,adress,zipcode,img, city) 
+this.tel=tel;
+this.website=website;
+}
+display() { 
+return `<div class="media  p-3">
+        <img src=${this.img} alt="" class="mr-3 mt-3 rounded-circle" style="width:150px;height:150px;">
+        <div class="media-body">
+            <div class="text-container pl-5 pt-10">
+                <h4>${this.location}</h4><br>
+                 <p>${this.description}</p>
+                 <div id="place-type">
+                 ${this.adress}<br>${this.zipcode}<br>${this.city}<br>${this.tel}<br>${this.website}
+                 </div>
+            </div>
+        </div>`
        
-   }
+}
    
 }
-var lemon=new Restaurants (`Lemon`,`A`,`Kettenbrückengasse 19`,1050,`../img/zoo.jpg`,`Vienna`,0660443322,`www.lemon.at`);
- var sixta=new Restaurants (`Sixta`,`T`,`Schönbrunner Strasse 21`, 1050, `../img/sixta.png`,`Vienna`,"0134523", `http://www.sixta.at`);
+var lemon=new Restaurants (`Lemon`,`At Lemon we are constantly searching for the aromas and flavors of Austria. Join us in the world of Lemon, share our passion and let us present our country’s harvest in the most delicious light.`,`Kettenbrückengasse 19`,1050,`../img/lemon.png`,`Vienna`,`0660443322`,`http://www.lemonleaf.at`);
+var sixta=new Restaurants (`Sixta`,`The sensitivity between creative cuisine and a traditional, Viennese offer determines our cuisine. We attach great importance to product quality, production quality and the best possible taste. The changing menu takes seasonal specialities and delicacies into account.`,`Schönbrunner Strasse 21`,1050,`../img/sixta.png`,`Vienna`,`0660443323`,`http://www.sixta-restaurant.at/`);
 
 var arrRest= [lemon,sixta]; 
 for (let i = 0; i < arrRest.length; i++) {
-  document.getElementsByClassName("rest-container")[i].innerHTML +=arrRest[i].display();
+document.getElementsByClassName("rest-container")[i].innerHTML +=arrRest[i].display();
 }
 
 class Events extends Loc { 
-   date:""; 
-   price:"";   
-constructor(location, description,adress,zipcode,img,city,tel,website,date,price) {
-       super(location, description,adress,zipcode,img,city,tel,website) 
-       this.date=date;
-       this.price=price;
-   }
-   display() { 
-       return`${this.location},${this.description},${this.adress},${this.zipcode},${this.img},${this.city},${this.website},${this.date},${this.price}`;
-     
-   }
+date:""; 
+price:"";   
+constructor(location,description,adress,zipcode,img,city,tel,website,date,price) {
+super(location,description,adress,zipcode,img,city,tel,website) 
+this.website=website;
+this.date=date;
+this.price=price;
 }
-var kris=new Events(`Kris Kristofferson`,`K`,`Wiener Stadthalle, Halle F, Roland Rainer Platz 1`, 1150, `./img/kris.jpg`,`Vienna`,`kriskristofferson.com/`,`Fr., 15.11.2019. 20:00`,`58,50 EUR`);
-var lenny=new Events(`Lenny Kravitz`,`L`,`Wiener Stadthalle - Halle D, Roland Rainer Platz 1`, 1150,`./img/lenny.jpg`,`Vienna`,`lennykravitz.com`,`Sat,09.12.2019,19:30`,`47,80 EUR`);
+display() { 
+return `<div class="media  p-3">
+        <img src=${this.img} alt="" class="mr-3 mt-3 rounded-circle" style="width:150px;height:150px;">
+        <div class="media-body">
+            <div class="text-container pl-5 pt-10">
+                <h4>${this.location}</h4><br>
+                 <p>${this.description}</p>
+                 <div id="place-type">
+                 ${this.adress}<br>${this.zipcode}<br>${this.city}<br>${this.website}<br>${this.date}
+                 </div>
+            </div>
+        </div>`
+     
+}
+}
+var kris=new Events(`Kris Kristofferson`,`Kristoffer Kristofferson (born June 22, 1936) is an American singer-songwriter and actor. Among his songwriting credits are the songs "Me and Bobby McGee", "For the Good Times", "Sunday Mornin' Comin' Down", and "Help Me Make It Through the Night", all of which were hits for other artists.`,`Wiener Stadthalle, Halle F, Roland Rainer Platz 1`, 1150,`../img/kris.jpg`,`Vienna`,``,`http://kriskristofferson.com/`,`Fr.,15.11.2019. 20:00`,`58,50 EUR`);
+
+var lenny=new Events(`Lenny Kravitz`,`Leonard Albert Kravitz (born May 26, 1964) is an American singer, songwriter, actor, record producer, and multi-instrumentalist. His "retro" style incorporates elements of rock, blues, soul, R&B, funk, jazz, reggae, hard rock, psychedelic, pop, folk, and ballads.`,`Wiener Stadthalle - Halle D, Roland Rainer Platz 1`, 1150,`../img/lenny.jpg`,`Vienna`,``,
+  `http://www.lennykravitz.com/`,`Sat,09.12.2019,19:30`,`47,80 EUR`);
 
 
 var arrEve= [lenny,kris]; 
 for (let i = 0; i < arrEve.length; i++) {
-  document.getElementsByClassName("eve-container")[i].innerHTML +=arrEve[i].display();
+document.getElementsByClassName("eve-container")[i].innerHTML +=arrEve[i].display();
 }
